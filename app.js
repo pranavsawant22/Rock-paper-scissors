43141a2ba65f15c9c0f28efa64fd79c4ddfa29f2
Pranav <pranavsawant1212@gmail.com>
2020-08-02 04:11:37 +0530

    final commit

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var userScore_span = document.getElementById('user-score');
 var compScore_span = document.getElementById('comp-score');
 const scoreboard_div = document.querySelector('.score-board');
 var result_div = document.querySelector('.result1')
-const rock_div = document.getElementById('r');
-const paper_div = document.getElementById('p');
-const scissor_div = document.getElementById('s');
+var rock_div = document.getElementById('r');
+var paper_div = document.getElementById('p');
+var scissor_div = document.getElementById('s');
 function convert(something){
     if (something=='r') return 'Rock';
     else if(something=='s') return 'Scissors';
@@ -23,14 +23,19 @@ compScore_span.innerHTML = compscore
 
 
 result_div.innerHTML = convert(user).fontcolor('green') + " beats " + convert(comp).fontcolor('red') + " YOU WIN!!!"
+
+document.getElementById(user).classList.add('green-glow')
+setTimeout(function(){document.getElementById(user).classList.remove('green-glow')},1900)
 }
+
 function lose(user,comp){
 
     compscore++;
     userScore_span.innerHTML = userScore
 compScore_span.innerHTML = compscore
 result_div.innerHTML = convert(comp).fontcolor('red') + " beats " + convert(user).fontcolor('green') + " YOU LOSE!!!"
-
+document.getElementById(user).classList.add('red-glow')
+setTimeout(function(){document.getElementById(user).classList.remove('red-glow')},1900)
 }
 function draw(){
     userScore_span.innerHTML = userScore
